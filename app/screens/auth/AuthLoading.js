40c302ee108c8846920b9cc80732a6dc164b97c5
Zakyyy <ahmed.abdelghafour@student.guc.edu.eg>
2020-04-25 21:33:24 +0200

    hagat aktar mn aly fato

--- app/screens/auth/AuthLoading.js
+++ app/screens/auth/AuthLoading.js
@@ -4,13 +4,8 @@ import AsyncStorage from '@react-native-community/async-storage';
 
 export default class AuthLoading extends React.Component {
   async componentDidMount() {
-<<<<<<< HEAD
     // await AsyncStorage.clear()
     let log_in = await AsyncStorage.getItem('logged_in')
-=======
-    //await AsyncStorage.clear();
-    let log_in = await AsyncStorage.getItem('logged_in');
->>>>>>> e6a839d9b737ef181411f5cafd2eea8539096aea
     if (log_in === 'true') {
       this.props.navigation.navigate('App');
     } else {
